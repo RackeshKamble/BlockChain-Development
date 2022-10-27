13646be14fa590352e90ab9342f166ab25e3c941
RAKESH KAMBLE <rakeshkamble@hotmail.com>
2022-10-27 21:01:50 +0530

    5.Token Transfer

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,28 +10,54 @@ contract Token{
     //Symbol
     string public symbol;
 
-    //Decimals
-    //unsigned integer which is not negative
-    //18 zeros in gwei
+    //Decimals - Unsigned integer which is not negative 18 zeros in gwei
     uint256 public decimals;
 
-    //Total Supply
-    //10 millions 10 to exponent of decimal value i.e. 18
-    //Gwie value
+    //Total Supply 10 millions 10 to exponent of decimal value i.e. 18 Gwie value
     uint256 public totalSupply;
 
     //Track Balance
     //mapping is datastructure holds key-value pair
     mapping(address => uint256) public balanceOf;
 
+    event Transfer(
+        address indexed from, 
+        address indexed to, 
+        uint256 value
+        );
+
     //Send Tokens
 
-    constructor(string memory _name, string memory _symbol, uint256 _decimals ,uint256 _totalSupply){
+    constructor(
+        string memory _name, 
+        string memory _symbol, 
+        uint256 _decimals,
+        uint256 _totalSupply
+        ) {
         name=_name;
         symbol=_symbol;
         decimals=_decimals;
         totalSupply = _totalSupply * (10**decimals);
         balanceOf[msg.sender]= totalSupply;
+    }
+
+    function transfer(address _to , uint256 _value) 
+    public returns(bool success)
+    {
+        //Require that spender has enough tokens to spend
+        require(balanceOf[msg.sender] >= _value);
+        //Check invalid receipent
+        //i.e.  0x0000000000000000000000000000000000000000
+        require(_to != address(0));
+
+        //Dedcut tokens from spender
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        //Credit tokens to receiver
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        //Emit Transfer Event
+        emit Transfer(msg.sender , _to ,_value);
+        return true;
     }
 }
 
--- test/Token.js
+++ test/Token.js
@@ -7,10 +7,9 @@ const tokens = (number) =>{
 
 describe("Token", ()=> {
 
-    let token, accounts, deployer;
+    let token, accounts, deployer, receiver;
     beforeEach(async()=>{
         
-        //Duplicate /reusble code goes here
         //Fetch Token from Blockchain using ethers.js by deploying it first
         const Token = await ethers.getContractFactory("Token");
         token = await Token.deploy("Rakesh's Token","RTBM","18","1000000");
@@ -18,9 +17,11 @@ describe("Token", ()=> {
         //Fetch Accounts
         accounts = await ethers.getSigners();
         deployer = accounts[0];
+        receiver = accounts[1];
 
     })
 
+    //Read Functions
     describe("Deployment", ()=>{
 
         const name="Rakesh's Token";
@@ -29,7 +30,7 @@ describe("Token", ()=> {
         const totalSupply=tokens("1000000");
 
         //Tests Written here
-         //Chai EXPECT used
+        //Chai EXPECT used
 
         it("has a name", async ()=>{        
             //Check if name is correct           
@@ -53,9 +54,55 @@ describe("Token", ()=> {
 
         it("assigns total supply to a developer", async ()=>{
             //Check if balance is correct
-            //console.log(deployer.address);
             expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
         })
     }) 
 
+    //Write Functions
+    describe("Sending Tokens", () => {
+
+        let amount,transaction,result;
+
+        describe("Success", async ()=>{
+            beforeEach(async ()=>{
+                //Transfer Token
+                amount = tokens(100);
+                transaction = await token.connect(deployer).transfer(receiver.address, amount);
+                result = await transaction.wait();
+            })
+    
+            it("trasnfer token balance", async () => {
+          
+                expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900));            
+                expect(await token.balanceOf(receiver.address)).to.equal(amount);
+                //Ensure Tokens were transfered(Balance Change)
+            })
+    
+            it("emits a transfer event", async ()=> {
+                const event = result.events[0];            
+                expect(event.event).to.equal("Transfer");
+                
+                const args = event.args;
+                expect(args.from).to.equal(deployer.address);
+                expect(args.to).to.equal(receiver.address);
+                expect(args.value).to.equal(amount);
+            })
+        })
+
+        describe("Failure", async()=>{
+            it("rejects insufficient balance", async()=>{
+                //Transfer more tokens than deployer has - 100M
+                const invalidAmount= tokens(100000000);
+                //Waffle assertion to throw erros .to.be.reverted
+                await expect(token.connect(deployer).transfer(receiver.address,invalidAmount)).to.be.reverted;
+            })
+
+            it("rejects insufficient recipent", async()=>{
+                //Invalid Recipent dummy zero address
+                const amount= tokens(100);
+                //Waffle assertion to throw erros .to.be.reverted
+                await expect(token.connect(deployer).transfer("0x0000000000000000000000000000000000000000",amount)).to.be.reverted;
+            })
+        })        
+    })
 })
